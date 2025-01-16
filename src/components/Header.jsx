d49b87ba900a947ce262573a558dcefcccb3485a
Ms Pickles1014 <177317399+MsPickles1014@users.noreply.github.comgit config --global user.emailgit config --global user.email noeladnelson@gmail.com>
2025-01-15 21:03:33 -0500

    code for NavBar.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+// import {link} from "react-router-dom"
+import Navbar from "./Header";
+
+function Header () {
+    return (
+    
+        <div>
+          <Navbar />
+     
+         
+        </div>
+  
+    );
+    
+}
+
+
+export default Header;
---
+++ src/pages/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const About = () => {
+
+return (
+<section id="about" className="page-section">
+<h2> About Me </h2>
+<div>
+<p> I am Noela, an experienced educator with a passion for fostering learning and development in students. 
+    After several years as an Executive Director of Education, I discovered a growing interest in technology and coding. 
+    Driven by a desire to embrace new challenges, I am now transitioning to a career as an aspiring software developer. 
+    I am honing my skills in programming languages like Python and JavaScript and exploring various software development frameworks. 
+    With a strong foundation in communication and problem-solving, I aim to create innovative solutions that enhance user 
+    experiences and contribute to meaningful projects in the tech industry.</p>
+ <p>I specialize in front-end and full-stack web development, with a focus on creating efficient, user-friendly applications. 
+    Below are some of the projects I’ve worked on so far. Let’s connect and build something amazing!</p>
+</div>
+</section>
+
+)
+
+}
+
+export default About;
